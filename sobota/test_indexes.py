66d51f654c81a94484983ecf4ac044a18755a912
Pavel Rusnok <pavel.rusnok@gmail.com>
2024-02-25 13:37:48 +0100

    nedele konec 2

--- sobota/test_indexes.py
+++ sobota/test_indexes.py
@@ -8,7 +8,4 @@ def test_strings():
 def test_integers():
     assert odd_indexes(3456) == "46"
 
-@pytest.mark.parametrize("test_input,expected",
-                         [("elephant", "lpat"), ("computer", "optr"), (3456, "46")])
-def test_str_and_int(test_input, expected):
-    assert odd_indexes(test_input) == expected
+
